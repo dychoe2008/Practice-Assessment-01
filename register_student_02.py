1460341c4956624b68c9f43172b4508bfc66b7dc
dychoe2008 <dychoe2008@gmail.com>
2024-05-07 19:09:02 +1200

    stuff

--- register_student_02.py
+++ register_student_02.py
@@ -1,6 +1,35 @@
 """The registering a student for living springs camp
-Make a funtion to 
+Make a funtion to register a student, make a string checker
 By Daniel
 """
 
+def string_checker(string, question):
+    """Check if the input is in the alphahbet"""
+    invaild = "\nSorry please enter a vaild option\n"
+    while string.isalpha() is False:
+        print (invaild)
+        string = str(input(question))
+    return string
 
+def registering():
+    """Funtion to ask for the student's name"""
+    invaild = "\nSorry please enter a vaild option\n"
+    student_registration = input("Would you like to register a student? (Y or N): ").title()
+    student_registration_confirm = string_checker(student_registration,
+                                              "Would you like to register a student?"
+                                               " (Y or N): " ).title()
+    if student_registration_confirm in NO:
+        #End the program here
+    elif student_registration_confirm in YES:
+        #Ask for age here
+    else:
+        print(f"{invaild}")
+        registering()
+
+
+YES = ["Yes","Y","YES"]
+NO = ["NO","No","N"]
+
+#Main Routine
+
+registering()
--- welcome_screen_01.py
+++ welcome_screen_01.py
@@ -3,9 +3,18 @@ Make a welcome screen, create constants,
 make the big loop, make some variables
 By Daniel :D
 """
+def welcome_screen():
+    """Welcome screen title"""
+    go = True
+    while go:
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        print("******** Living Springs Holiday Camp - registration ********")
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+
+
 SHUTTLE_COST = 80
 NO_SHUTTLE_COST = 0
-YES_OR_NO = ["Yes","Y","YES","NO","No","N"]
 MEALS = [["1. Standard",],
          ["2. Vegan",],
          ["3. Vegetarian"]
@@ -14,10 +23,3 @@ CAMP_OPTIONS = [["1.Walking and social skills",5,"easy",800],
                 ["2.Kayaks and swimming",3,"moderate",400],
                 ["3.Tramping and biking",4,"difficult",900],
                 ]
-yes = True
-
-
-while yes:
-    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-    print("******** Living Springs Holiday Camp - registration ********")
-    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
