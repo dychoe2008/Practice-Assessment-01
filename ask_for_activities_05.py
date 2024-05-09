f9f4458494eacaacb60f21800c9b203d8f404db3
dychoe2008 <dychoe2008@gmail.com>
2024-05-09 19:21:13 +1200

    Create ask_for_activities_05.py

---
+++ ask_for_activities_05.py
@@ -0,0 +1,7 @@
+"""Ask for what activities they want sign up for
+Show the list of activities, ask for which one they want,
+make an integer checker, check the range of the integer
+By Daniel C
+"""
+
+
